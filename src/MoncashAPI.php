f3a0ac6839992a186530b5bb4173f386dbb2dd02
schery19 <44877783+schery19@users.noreply.github.com>
2023-05-30 07:46:25 -0400

    Update MoncashAPI.php

--- src/MoncashAPI.php
+++ src/MoncashAPI.php
@@ -66,23 +66,27 @@ class MoncashAPI {
 
 		$url = $url_split[0]."//".$this->getCredentials()->getClient_id().":".$this->getCredentials()->getClient_secret()."@".$url_split[1]."".Constants::OAUTH_TOKEN_URI;
 
-		$httpClient = new \Guzzle\Http\Client();
+		$httpClient = new \GuzzleHttp\Client(['verify'=>false]);
 
 		try {
 
-	 		$req = $httpClient->post($url, array('Accept' =>"application/json"), array('scope'=>"read,write", 'grant_type'=>"client_credentials"));
+	 		//$req = $httpClient->post($url, array('Accept' =>"application/json"), array('scope'=>"read,write", 'grant_type'=>"client_credentials"));
 
-	 		$res = $req->send();
+			 $req = $httpClient->post($url, array(
+				"form_params"=>array('scope'=>"read,write", 'grant_type'=>"client_credentials"),
+				"headers"=>array('Accept' =>"application/json")
+			 ));
 
-	 		return json_decode($res->getBody(), true);
+			 $res = $req->getBody()->getContents();
+
+	 		return json_decode($res, true);
 
 
-	 	} catch(\Guzzle\Http\Exception\ClientErrorResponseException $e) {
+	 	} catch(\GuzzleHttp\Exception\ClientException $e) {
 	 		throw new MoncashException("Impossible de s'authentifier");
 	 	}
 
 	}
-
 
 
 	public function makePaymentRequest($order_id, $amount) {
@@ -94,19 +98,25 @@ class MoncashAPI {
 
 		$order = array('amount'=>"$amount", 'orderId'=>"$order_id");
 
-		$httpClient = new \Guzzle\Http\Client();
+		$httpClient = new \GuzzleHttp\Client(['verify'=>false]);
 
 		try {
 
-	 		$req = $httpClient->post($url, array('Accept' =>"application/json", 'authorization'=>"Bearer $this->token",'Content-type'=>"application/json"), json_encode($order));
+			 $req = $httpClient->post($url, array(
+				"headers"=>array(
+					'Accept' =>"application/json", 
+					'authorization'=>"Bearer $this->token",
+					'Content-type'=>"application/json"),
+				"body"=>json_encode($order)
+			 ));
 
-	 		$res = $req->send();
+			 $res = $req->getBody()->getContents();
 
-	 		$details = json_decode($res->getBody(), true);
+	 		$details = json_decode($res, true);
 
 	 		return new PaymentRequest($this->credentials, $details);
 
-	 	} catch(\Guzzle\Http\Exception\ClientErrorResponseException $e) {
+	 	} catch(\GuzzleHttp\Exception\ClientException $e) {
 	 		throw new MoncashException("Impossible d'effectuer le paiement");
 	 	}
 
@@ -120,17 +130,23 @@ class MoncashAPI {
 
 		$transfert = array("amount"=>$amount, "receiver"=>$receiver, "desc"=>$desc);
 
-		$httpClient = new \Guzzle\Http\Client();
+		$httpClient = new \GuzzleHttp\Client(['verify'=>false]);
 
 		try {
 
-	 		$req = $httpClient->post($url, array('Accept' =>"application/json", 'authorization'=>"Bearer $this->token",'Content-type'=>"application/json"), json_encode($transfert));
+			 $req = $httpClient->post($url, array(
+				"headers"=>array(
+					'Accept' =>"application/json", 
+					'authorization'=>"Bearer $this->token",
+					'Content-type'=>"application/json"),
+				"body"=>json_encode($transfert)
+			 ));
 
-	 		$res = $req->send();
+	 		$res = $req->getBody()->getContents();
 
-	 		return new Transfert(json_decode($res->getBody(), true));
+	 		return new Transfert(json_decode($res, true));
 
-	 	} catch(\Guzzle\Http\Exception\ClientErrorResponseException $e) {
+	 	} catch(\GuzzleHttp\Exception\ClientException $e) {
 	 		throw new MoncashException("Impossible d'effectuer le transfert");
 	 	}
 	}
@@ -140,21 +156,26 @@ class MoncashAPI {
 
 		$url = $this->credentials->getConfigs()['api_endpoint'].Constants::PAYMENT_ORDER_URI;
 
-
 		$order = array("orderId"=>$order_id);
 
-		$httpClient = new \Guzzle\Http\Client();
+		$httpClient = new \GuzzleHttp\Client(['verify'=>false]);
 
 		try {
 
-	 		$req = $httpClient->post($url, array('Accept' =>"application/json", 'authorization'=>"Bearer $this->token",'Content-type'=>"application/json"), json_encode($order));
+			 $req = $httpClient->post($url, array(
+				"headers"=>array(
+					'Accept' =>"application/json", 
+					'authorization'=>"Bearer $this->token",
+					'Content-type'=>"application/json"),
+				"body"=>json_encode($order)
+			 ));
+	 		
+			$res = $req->getBody()->getContents();
 
-	 		$res = $req->send();
-
-	 		return new PaymentDetails(json_decode($res->getBody(), true));
+	 		return new PaymentDetails(json_decode($res, true));
 
 
-	 	} catch(\Guzzle\Http\Exception\ClientErrorResponseException $e) {
+	 	} catch(\GuzzleHttp\Exception\ClientException $e) {
 	 		throw new MoncashException("Impossible de trouver cette transaction");
 	 	}
 
@@ -167,18 +188,24 @@ class MoncashAPI {
 
 		$transaction = array("transactionId"=>$transaction_id);
 
-		$httpClient = new \Guzzle\Http\Client();
+		$httpClient = new \GuzzleHttp\Client(['verify'=>false]);
 
 		try {
 
-	 		$req = $httpClient->post($url, array('Accept' =>"application/json", 'authorization'=>"Bearer $this->token",'Content-type'=>"application/json"), json_encode($transaction));
+	 		$req = $httpClient->post($url, array(
+				"headers"=>array(
+					'Accept' =>"application/json", 
+					'authorization'=>"Bearer $this->token",
+					'Content-type'=>"application/json"),
+				"body"=>json_encode($transaction)
+			 ));
+			
+			$res = $req->getBody()->getContents();
 
-	 		$res = $req->send();
-
-	 		return new PaymentDetails(json_decode($res->getBody(), true));
+	 		return new PaymentDetails(json_decode($res, true));
 
 
-	 	} catch(\Guzzle\Http\Exception\ClientErrorResponseException $e) {
+	 	} catch(\GuzzleHttp\Exception\ClientException $e) {
 	 		throw new MoncashException("Impossible de trouver cette transaction");
 	 	}
 
