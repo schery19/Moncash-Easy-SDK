910498d0a398885e399c4a4ac6f8ddd50deffce3
schery19 <44877783+schery19@users.noreply.github.com>
2023-06-01 12:19:33 -0400

    Update MoncashException.php

--- src/MoncashException.php
+++ src/MoncashException.php
@@ -2,12 +2,36 @@
 
 namespace MoncashEasy\SDK;
 
-use \Exception;
+use \Exception as Exception;
+use \GuzzleHttp\Exception\ClientException as GuzzleException;
 
 
 class MoncashException extends Exception {
 	
-	
+	public function __construct($e) {
+        
+		if($e instanceof GuzzleException) {
+
+		    $response = $e->getResponse();
+
+		    $this->code = $response->getStatusCode();
+
+		    switch($response->getStatusCode()) {
+			case 401 :
+			    $this->message = "Impossible de s'authentifier";
+			    break;
+			case 404 :
+			    $this->message = "Ressource introuvable";
+			    break;
+			default :
+			    $this->message = $e->getMessage();
+			    break;
+		    }
+
+		} else {
+		    $this->message = $e;
+		} 
+   	 }
 }
 
 ?>
