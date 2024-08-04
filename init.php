583e6e62b84bf52a66ad9b783440c7a40d859a36
schery19 <44877783+schery19@users.noreply.github.com>
2024-08-04 19:12:49 -0400

    Add files via upload

---
+++ init.php
@@ -0,0 +1,24 @@
+<?php
+
+require __DIR__.'/src/Configuration.php';
+
+require __DIR__.'/src/Constants.php';
+
+require __DIR__.'/src/Credentials.php';
+
+require __DIR__.'/src/MoncashAPI.php';
+
+require __DIR__.'/src/MoncashException.php';
+
+require __DIR__.'/src/Payment.php';
+
+require __DIR__.'/src/PaymentDetails.php';
+
+require __DIR__.'/src/PaymentRequest.php';
+
+require __DIR__.'/src/RequestHandler.php';
+
+require __DIR__.'/src/Transfert.php';
+
+
+?>
